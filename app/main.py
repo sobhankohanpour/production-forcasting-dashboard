f2ddc1f00e96185e76c823243cb974c4834e21bf
Sobhan Kohanpour <kohanpour.sobhan@gmail.com>
2025-11-21 14:48:32 +0330

    Add real 'North Dakota Natural Gas Production' dataset🛢 and improve dashboard UI/details

--- app/main.py
+++ app/main.py
@@ -7,11 +7,12 @@ save_dir = os.path.join("data")
 # Ensure the folder exists
 os.makedirs(save_dir, exist_ok=True)
 
-home, upload_dataset, preprocessing_tab, train_ml_model = st.tabs([
+home, upload_dataset, preprocessing_tab, train_ml_model, prediction_tab = st.tabs([
     "🏠 Home", 
     "📁 Select/Upload Dataset", 
     "🛠️ Preprocessing", 
-    "🤖 Train ML Model"
+    "🤖 Train ML Model",
+    "Prediction"
 ])
 
 with home:
@@ -34,7 +35,18 @@ with home:
         "visualizations, and AI-powered predictions designed for real-world petroleum engineering workflows."
         )
 with upload_dataset:
+    st.info(
+        """
+        You can select from the default datasets included in the project,
+        or upload your own custom datasets to use with the models.
+        """
+    )
     if st.toggle("I want to upload my dataset."):
+        st.info(
+            """
+            You can upload only `.xlsx` files.  
+            """
+        )
         number_of_user_dataset = st.slider(
             "Number of dataset(s)", min_value=1, max_value=50, value=1
         )
@@ -55,8 +67,26 @@ with upload_dataset:
                 df_uploaded_file.to_excel(save_path, index=False)
                 st.success(f"Dataset #{i+1} saved.")
 
+    if st.toggle("I want to select from the real-word datasets."):
+        st.checkbox(
+            "North Dakota Natural Gas Production", 
+            help="This dataset comes from North Dakota and contains real-world data. " \
+            "It includes monthly values for gas produced, sold, and flared, along with " \
+            "additional measurements specific to the Bakken formation. Because the data reflects" \
+            " actual field-level reporting, it is suitable for analytics, forecasting, and operational studies.",
+            key="nd_dataset",
+            )
+        
+
 with preprocessing_tab:
-    st.write("Data preprocessing...")
+    st.info("Here, you can process the dataset before training your model on it.")
 
 with train_ml_model:
-    st.write("Training the ML model...")
+    st.info(
+        "Here, you can train machine learning models on the selected dataset, " \
+        "configure the hyperparameters, and monitor the training process to achieve " \
+        "the best performance."
+        )
+
+with prediction_tab:
+    st.info("Here, you can predict based on the trained model.")
