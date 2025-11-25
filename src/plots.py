eea95ae5b971d81d02688317846386c74790ff60
Sobhan Kohanpour <kohanpour.sobhan@gmail.com>
2025-11-25 17:21:57 +0330

    add lineplot() and ensure it renders in tested dashboard

--- src/plots.py
+++ src/plots.py
@@ -7,4 +7,10 @@ def scatterplot(dataset, x, y, hue=None, title="Scatter Plot"):
     fig, ax = plt.subplots()
     sns.scatterplot(data=dataset, x=x, y=y, hue=hue, ax=ax)
     ax.set_title(title)
-    st.pyplot(fig)  
+    st.pyplot(fig)
+
+def lineplot(dataset, x, y, hue=None, title="Line Plot"):
+    fig, ax = plt.subplots()
+    sns.lineplot(data=dataset, x=x, y=y, hue=hue, ax=ax)
+    ax.set_title(title)
+    st.pyplot(fig)
